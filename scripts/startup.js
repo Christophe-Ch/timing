977db5210fc8b2dfa07cef24dd5aa8170f50d5a6
Christophe Chichmanian <chris.chich@hotmail.fr>
2019-03-23 19:41:36 +0100

    Startup page done :sparkles:

--- scripts/startup.js
+++ scripts/startup.js
@@ -1,10 +1,32 @@
-// chrome.browserAction.setPopup({popup: "views/popup.html"});
-
 const pickButton = document.getElementById('continue-btn');
-const startButton = document.getElementById('continue-btn');
+const startButton = document.getElementById('start-btn');
 const dateField = document.getElementById('date');
 
-let date = new Date();
+dateField.valueAsDate = new Date();
 
-dateField.nodeValue = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`;
-console.log(`${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`);
+// Events
+const setDefaultDate = (dateValue) => {
+    let date = null;
+
+    if(!dateValue) {
+        date = (new Date()).getTime();
+    }
+    else {
+        date = (new Date(dateValue)).getTime();
+    }
+    
+    chrome.storage.sync.set({startTime: date}, () => {
+        chrome.browserAction.setPopup({popup: "views/popup.html"});
+    });
+
+    window.location.href = "popup.html";
+}
+
+pickButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    setDefaultDate(dateField.value);
+});
+
+startButton.addEventListener('click', () => {
+    setDefaultDate();
+});
