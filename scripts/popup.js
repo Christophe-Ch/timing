59b938ef460e298e50e7080fa9826f47cde588cc
Christophe Chichmanian <chris.chich@hotmail.fr>
2019-03-24 16:55:22 +0100

    Added clearing feature :sparkles:

--- scripts/popup.js
+++ scripts/popup.js
@@ -16,6 +16,7 @@
 // });
 
 const table = document.getElementById('table-body');
+const clear = document.getElementById('clear-btn');
 
 chrome.storage.sync.get('data', (result) => {
     result.data.forEach(element => {
@@ -28,4 +29,9 @@ chrome.storage.sync.get('data', (result) => {
         from.innerHTML = element.from;
         to.innerHTML = element.to;
     });
+});
+
+clear.addEventListener('click', () => {
+    chrome.storage.sync.set({data: []});
+    location.reload();
 });
